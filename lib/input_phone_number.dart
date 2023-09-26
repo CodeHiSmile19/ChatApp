7237ea37eee70574876c393517fd715c788f7bad
HiSmile <flutterdeveloper98@gmail.com>
2023-09-26 21:20:14 +0700

    feat: init project

---
+++ lib/input_phone_number.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class InputPhoneNumber extends StatefulWidget {
+  const InputPhoneNumber({Key? key}) : super(key: key);
+
+  @override
+  State<InputPhoneNumber> createState() => _InputPhoneNumberState();
+}
+
+class _InputPhoneNumberState extends State<InputPhoneNumber> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 14,
+                  left: 16,
+                ),
+                child: SvgPicture.asset(
+                  "assets/vectors/ic_arrow_left.svg",
+                ),
+              ),
+              const SizedBox(height: 98),
+              const Center(
+                child: Text(
+                  "Enter Your Phone Number",
+                  style: TextStyle(
+                    color: Color(0xFF0F1828),
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              SizedBox(height: 48),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 36,
+                      width: 74,
+                      margin: const EdgeInsets.only(right: 8),
+                      decoration: BoxDecoration(
+                        color: Colors.yellow, // Color(0xFFF7F7FC),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    ),
+
+                    ///Bật bàn phím trên máy ảo: Ctrl + K
+                    Expanded(
+                      child: Container(
+                        height: 36,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF7F7FC),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 8,
+                          vertical: 4,
+                        ),
+                        child: TextFormField(
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            hintText: "Phone Number",
+                            hintStyle: TextStyle(
+                              color: Color(0xFFADB5BD),
+                              fontSize: 14,
+                            ),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+import 'input_phone_number.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const InputPhoneNumber(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 135,
+                    bottom: 42,
+                  ),
+                  child: Image.asset(
+                    'assets/images/img_background.png',
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 48,
+                  ),
+                  child: Text(
+                    'Connect easily with your family and friends over countries',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Text(
+            'Terms & Privacy Policy',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          Container(
+            margin: const EdgeInsets.only(
+              top: 18,
+              bottom: 20,
+              left: 24,
+              right: 24,
+            ),
+            height: 52,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: const Color(0xFF002ED3),
+            ),
+            child: const Center(
+              child: Text(
+                "Start Messaging",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
