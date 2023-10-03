a1eed88efa46aec5a9a1ceea764ac61f01cb0701
HiSmile <flutterdeveloper98@gmail.com>
2023-10-03 21:10:45 +0700

    feat: list view in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'home_chat_screen.dart';
 import 'input_phone_number.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InputPhoneNumber(),
+      home: const HomeChatScreen(),
     );
   }
 }
